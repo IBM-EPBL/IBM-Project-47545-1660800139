1abfb7034b0871a5c4598460933a79df90d9edb8
hosanna325 <82701010+hosanna325@users.noreply.github.com>
2022-11-18 23:49:24 +0530

    Add files via upload

---
+++ Project Development Phase/Sprint-4/Static/app.js
@@ -0,0 +1,7 @@
+const selectedItem = document
+  .getElementById('sidebarCollapse')
+  .addEventListener('click', (e) => {
+    const ele = document.getElementById('sidebar');
+    ele.classList.toggle('active');
+  });
+Footer
